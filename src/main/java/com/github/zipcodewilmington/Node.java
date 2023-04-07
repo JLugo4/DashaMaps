e51da0ef99560e36de1986b9c295b449199f6fa2
Jarek <jarek@Zipcoders-MacBook-Pro-65.local>
2023-04-07 16:10:43 -0400

    Started

---
+++ src/main/java/com/github/zipcodewilmington/Node.java
@@ -0,0 +1,26 @@
+package com.github.zipcodewilmington;
+
+public class Node<T> {
+    String data;
+    Node<String> next;
+
+    public Node(String data){
+        this.data = data;
+        next = null;
+    }
+    public String getData(){
+        return data;
+    }
+
+    public void setData(String data){
+        this.data = data;
+    }
+
+    public Node<String> getNext(){
+        return next;
+    }
+
+    public void setNext(Node node){
+        this.next = node;
+    }
+}
